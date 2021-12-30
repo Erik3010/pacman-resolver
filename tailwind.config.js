250eac9ffabb5198796d6f1257e8363be7aa89d1
Erik <erikhau3010@gmail.com>
2021-12-31 00:29:47 +0700

    feat: initial

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        main: "rgba(0, 112, 243, 0.188)",
+      },
+    },
+  },
+  plugins: [],
+};
