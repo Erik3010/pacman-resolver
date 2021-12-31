770fa36d92c22fb08140e7d7a6872b0feca71386
Erik <erikhau3010@gmail.com>
2021-12-31 23:58:21 +0700

    feat: add content to create page

--- src/views/Create.vue
+++ src/views/Create.vue
@@ -1,5 +1,8 @@
 <template>
-  <div>Create Page</div>
+  <div class="text-center">
+    <h1 class="font-semibold text-2xl">Setup Your Board</h1>
+    <p class="text-gray-400 text-sm">Define your board row and column</p>
+  </div>
 </template>
 
 <script setup lang="ts"></script>
