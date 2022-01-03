5e079167a1d792eb3ce37f2ae5e2550046cabc1f
Erik <erikhau3010@gmail.com>
2022-01-04 00:03:12 +0700

    feat: add gradient to text

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,6 +1,18 @@
 <template>
   <section class="max-w-2xl w-full mx-auto text-center">
-    <h1 class="font-bold text-5xl sm:text-7xl mb-2">Pacman Resolver</h1>
+    <h1 class="font-bold text-5xl sm:text-7xl mb-2">
+      Pacman
+      <span
+        class="
+          bg-gradient-to-br
+          from-cyan-500
+          to-blue-600
+          text-transparent
+          bg-clip-text
+        "
+        >Resolver</span
+      >
+    </h1>
     <p class="text-md text-gray-400">Resolve your pacman maze</p>
     <RouterLink :to="{ name: 'Setup' }">
       <Button :classNames="['mt-12', 'mx-auto']">Start Resolve</Button>
