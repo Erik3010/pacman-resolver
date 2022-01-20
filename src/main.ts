ff9762dabdbea445c406cc83818f64908836b985
Erik <erikhau3010@gmail.com>
2022-01-21 01:56:10 +0700

    chore: remove vuex, add pinia

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { createApp } from "vue";
 import App from "@/App.vue";
 import router from "@/router";
 
+import { createPinia } from "pinia";
+
 import "@/styles/index.css";
 
-createApp(App).use(router).mount("#app");
+createApp(App).use(router).use(createPinia()).mount("#app");
