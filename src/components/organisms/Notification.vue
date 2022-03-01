8007dcef0f37e5bfe1eba309f6dbfc2d17724abb
Erik <erikhau3010@gmail.com>
2022-03-01 21:57:17 +0700

    refactor: change html attribute order

--- src/components/organisms/Notification.vue
+++ src/components/organisms/Notification.vue
@@ -6,12 +6,12 @@
     >
       <div class="mr-3">
         <svg
+          v-if="notificationStore.status === NotificationStatus.SUCCESS"
           xmlns="http://www.w3.org/2000/svg"
           class="h-6 w-6 text-green-600"
           fill="none"
           viewBox="0 0 24 24"
           stroke="currentColor"
-          v-if="notificationStore.status === NotificationStatus.SUCCESS"
         >
           <path
             stroke-linecap="round"
@@ -21,12 +21,12 @@
           />
         </svg>
         <svg
+          v-else-if="notificationStore.status === NotificationStatus.ERROR"
           xmlns="http://www.w3.org/2000/svg"
           class="h-6 w-6 text-red-500"
           fill="none"
           viewBox="0 0 24 24"
           stroke="currentColor"
-          v-else-if="notificationStore.status === NotificationStatus.ERROR"
         >
           <path
             stroke-linecap="round"
