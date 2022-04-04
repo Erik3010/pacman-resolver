4392f94452b7c6cfa6f116f98285abc34809c8a3
Erik <erikhau3010@gmail.com>
2022-04-04 12:01:10 +0700

    deployment

---
+++ assets/Button.463e13c0.js
@@ -0,0 +1,1 @@
+import{_ as a}from"./index.cad5a9e0.js";import{d as s,o as r,c as n,r as c,q as l}from"./vendor.cfaae494.js";const u=s({props:{classNames:null,color:{default:"primary"}},setup(e){const o=e;return(t,_)=>(r(),n("button",{class:l(["button",`button--${e.color}`,o.classNames])},[c(t.$slots,"default",{},void 0,!0)],2))}});var m=a(u,[["__scopeId","data-v-4abcc402"]]);export{m as B};
