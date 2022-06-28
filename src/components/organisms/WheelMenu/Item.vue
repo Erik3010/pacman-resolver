5297050d8edc275c00f494008201e939f7f53ef9
Erik <erikhau3010@gmail.com>
2022-06-28 09:12:53 +0700

    refactor: change class order

--- src/components/organisms/WheelMenu/Item.vue
+++ src/components/organisms/WheelMenu/Item.vue
@@ -4,7 +4,7 @@
     @click="emit('click')"
   >
     <div
-      class="absolute w-full h-full flex justify-center items-center text-2xlgit transform -rotate-45 select-none"
+      class="absolute w-full h-full flex justify-center items-center text-2xl transform -rotate-45 select-none"
     >
       <slot />
     </div>
